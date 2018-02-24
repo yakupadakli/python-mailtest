9ececc0e7744d45e56e6f9844b1774b0da31ac92
Yakup Adaklı <yakup.adakli@gmail.com>
2018-02-24 13:51:40 +0300

    Status check added.

--- mailtest/api.py
+++ mailtest/api.py
@@ -1,4 +1,5 @@
 from mailtest.client import Client
+from mailtest.errors import MailTestError
 
 
 class Api(Client):
@@ -11,5 +12,31 @@ class Api(Client):
         self.version = self.VERSION
         self.base_url = "%s%s" % (self.BASE_URL, self.version)
 
+        self._active = "active"
+        self._disposable = "disposable"
+        self._robot = "robot"
+        self._invalid = "invalid"
+        self._unknown = "unknown"
+        self._error = "error"
+        self.statuses = {
+            self._active: "ACTIVE",
+            self._disposable: "DISPOSABLE",
+            self._robot: "ROBOT",
+            self._invalid: "INVALID",
+            self._unknown: "UNKNOWN",
+            self._error: "ERROR",
+        }
+        self.codes = {
+            self._active: ["01"] ,
+            self._disposable: ["11"],
+            self._robot: ["12"],
+            self._invalid: ["21", "22", "23"],
+            self._unknown: ["81", "82"],
+            self._error: ["91", "92"],
+        }
+
     def check(self, domain):
-        return self._get(domain)
+        result = self._get(domain)
+        if result.get("status") not in self.statuses.values():
+            raise MailTestError
+        return result
